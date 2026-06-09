acce57ef1bff49814d9454850f152057ebbf7ea9
Somya Kumar <33091016+Saumya837@users.noreply.github.com>
2026-06-09 08:00:01 +0530

    feat: complete MyVector<T> implementation
    Implemented:
    - Constructors: default, size, size+value
    - Destructor: destroy elements + deallocate
    - _resize_internal: double capacity growth
    - push_back, pop_back, clear
    - resize: handles both grow and shrink
    - reserve: exact capacity allocation
    - operator[], front, back
    - size, capacity, empty
    - begin, end iterators
    
    Todo:
    - emplace_back (perfect forwarding - weekend)
    - fix: duplicate empty(), typo destroy_at, back() off-by-one

--- phase1-memory/03-MyVector/include/MyVector.h
+++ phase1-memory/03-MyVector/include/MyVector.h
@@ -12,7 +12,7 @@ class MyVector{
 
         void _resize_internal(){
             int num_elemets = _end - _start;
-            int new_capacity = 2 * (_capacity_end - _start);
+            int new_capacity = (_capacity_end == _start) ? 1 : 2 * (_capacity_end - _start);
 
             T* newArr = _alloc.allocate(new_capacity);
 
@@ -50,32 +50,83 @@ class MyVector{
         }
 
         // destructor
-        ~MyVector();
+        ~MyVector(){
+            if(_start == nullptr) return;  
 
-        
+            // need to destroy old objects first before freeing memory
+            for (T* p = _start; p != _end; ++p)
+                std::destroy_at(p);
 
-        // resize(int n){
-        //     //Todo: first look if we sufficiant space 
-        //     //Todo: if not call resize_internals to allocate 2*n size
-        //     //Todo: change _end to nth element
-        //     //Todo: later increase size assigning _size = _end - _start
-        // }
+            _alloc.deallocate(_start, _capacity_end - _start);
+        }
 
-        // push_back(int n){
 
-        // }
+        void resize(int n){
+            if(n > size()){
+                reserve(n);
+                std::uninitialized_fill(_end, _start + n, T{});
+            }
+            else{
+                //Shrinking to n size
+                for (T* p = _start + n; p != _end; ++p)
+                    std::destroy_at(p);
+            }
+            _end = _start + n;
+        }
+
+   
 
     // element access
-    T& operator[](int i);
-    T& at(int i);
-    T& front();
-    T& back();
+    T& operator[](int i){
+        int num_ele = _end - _start;
+        if(i < 0 || i >= num_ele){
+            throw std::runtime_error("Index out of Bounds");
+        }
+        return *(_start + i);
+    }
+
+    T& front(){
+        if(empty()) 
+            throw std::runtime_error("Vector is Empty");
+        return *(_start);
+    }
+
+    T& back(){
+        if(empty()) 
+            throw std::runtime_error("Vector is Empty");
+        return *(_end - 1);
+    }
 
     // modifiers
-    void push_back(T val);
+    void push_back(T val){
+        if(_end == _capacity_end){
+            if(_start == nullptr)
+                reserve(1);      // first allocation!
+            else
+                _resize_internal();
+        }
+        //memory is uninitialized, construct properly
+        std::__construct_at(_end, val);
+        _end++;
+    }
+
+    //need to learn perfect forwarding planned for weekends
     void emplace_back();
-    void pop_back();
-    void clear();
+
+    void pop_back(){
+        if(empty()) 
+            throw std::runtime_error("Vector is Empty");
+        std::destroy_at(_end - 1);
+        _end--;
+    }
+
+    void clear(){
+        if (empty()) return;
+        for(T* p = _start; p <_end; p++){
+            std::destroy_at(p);
+        }
+        _end = _start;
+    }
 
     // size & capacity
     size_t size(){
@@ -87,14 +138,35 @@ class MyVector{
     }
 
     bool empty(){
-        if(_end == _start)
-            return true;
-        return false;
+        return _start == _end;
     }
 
-    void reserve(int n);
+    void reserve(int n){
+        if(capacity() >= n) return;
+        else{
+           int old_size = _end - _start;
+           T* newVec =  _alloc.allocate(n);
+
+           //copy all the elements in the newArr
+           std::uninitialized_copy(_start, _end, newVec);
+
+           for (T* p = _start; p < _end; p++){
+                std::destroy_at(p);
+           }
+
+           _alloc.deallocate(_start, _capacity_end - _start);
+
+           _start = newVec;
+           _end = _start + old_size;
+           _capacity_end = _start + n;
+        }
+    }
 
     // iterators
-    T* begin();
-    T* end();
+    T* begin(){
+        return _start;
+    }
+    T* end(){
+        return _end;
+    }
 };
